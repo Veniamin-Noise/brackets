1a94d1b2fef6630cda0e2b31292030220254690f
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2023-01-23 11:13:51 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-module.exports = function check2(str, bracketsConfig) {
-    let breaketsCount = []
-    for (let j = 0; j < bracketsConfig.length; j++) {
-        breaketsCount = breaketsCount.concat(0);
-    }
-    let waitFor = [];
+module.exports = function check(str, bracketsConfig) {
+    if (str.length == 1) return false
 
-    let arr = str.split('');
-    for (let i = 0; i < arr.length; i++){
-        for (let j = 0; j < bracketsConfig.length; j++){
-            if (waitFor[waitFor.length - 1] == j && arr[i] == bracketsConfig[j][1]) {
-                waitFor.pop()
-                breaketsCount[j]--;
-            } else if(arr[i] == bracketsConfig[j][0]) {
-                breaketsCount[j]++;
-                waitFor.push(j);
-            } else if(arr[i] == bracketsConfig[j][1]) {
-                return false;
-            }
+    for (let i = 0; i < bracketsConfig.length; i++) {               // realisation '())('
+        if (str.includes(bracketsConfig[i][0] + bracketsConfig[i][1])) {
+            let clearStr = str.replace([bracketsConfig[i][0] + bracketsConfig[i][1]], '')
+            if (clearStr[0] == '}' || clearStr[0] == ')' || clearStr[0] == ']' || clearStr[-1] == '{' || clearStr[-1] == '(' || clearStr[-1] == '[' || str.length % 2 != 0) return false
         }
+        else {
+            let clearStr2 = str                                     // realisation '([{}])':
+            for (let b = 0; b <= str.length; b++) {
+                if (bracketsConfig.length == 2) clearStr2 = clearStr2.replace([bracketsConfig[0][0] + bracketsConfig[0][1]], '').replace([bracketsConfig[1][0] + bracketsConfig[1][1]], '')
+                else if (bracketsConfig.length == 3) clearStr2 = clearStr2.replace([bracketsConfig[0][0] + bracketsConfig[0][1]], '').replace([bracketsConfig[1][0] + bracketsConfig[1][1]], '').replace([bracketsConfig[2][0] + bracketsConfig[2][1]], '')
+                if (clearStr2 == '') return true
+            }
+            return false
+        }
+        return true
     }
-    for(let j = 0; j < bracketsConfig.length; j++) {
-        if (breaketsCount[j] != 0) return false;
-    }
-    return true;
+    return true
 }
